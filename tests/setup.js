439eae76efec1d5826d6a842727b4d49b3f74b4e
Neequaye-Ashaley <solomonnee02@gmail.com>
2026-03-07 22:07:17 +0000

    task manager rest api with authentication

--- tests/setup.js
+++ tests/setup.js
@@ -1,3 +1,9 @@
+import mongoose from "mongoose";
+import { cleanupDB } from "../src/config/db.js";
 import app from "../src/app.js";
+
+afterAll(async () => {
+  await cleanupDB();
+});
 
 export default app;
